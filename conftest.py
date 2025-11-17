51023434a577a4df5097d762f1140683ea2e4d8c
Asadbek Kodirov <asadbekkodirov@Asadbeks-MacBook-Pro.local>
2025-11-17 14:36:24 +0900

    conftest.py changed

--- conftest.py
+++ conftest.py
@@ -1,20 +1,23 @@
 import pytest
 import os
+
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.edge.service import Service as EdgeService
+
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
 
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 
-@pytest.fixture(params=["chrome", "firefox", "edge"])
+@pytest.fixture(params=["chrome", "firefox"])
 def driver(request):
     browser = request.param
-
-    # Detect if running in CI
     headless = os.getenv("CI") == "true"
 
     if browser == "chrome":
@@ -23,30 +26,20 @@ def driver(request):
             options.add_argument("--headless=new")
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
-        drv = webdriver.Chrome(
-            ChromeDriverManager().install(),
-            options=options
-        )
+
+        service = ChromeService(ChromeDriverManager().install())
+        drv = webdriver.Chrome(service=service, options=options)
 
     elif browser == "firefox":
         options = FirefoxOptions()
         if headless:
             options.add_argument("-headless")
-        drv = webdriver.Firefox(
-            executable_path=GeckoDriverManager().install(),
-            options=options
-        )
 
-    elif browser == "edge":
-        options = EdgeOptions()
-        if headless:
-            options.add_argument("--headless=new")
-            options.add_argument("--no-sandbox")
-            options.add_argument("--disable-dev-shm-usage")
-        drv = webdriver.Edge(
-            EdgeChromiumDriverManager().install(),
-            options=options
-        )
+        service = FirefoxService(GeckoDriverManager().install())
+        drv = webdriver.Firefox(service=service, options=options)
+
+    # Edge is DISABLED in CI because Microsoft blocks automated downloads
+    # If you want to enable locally, add param and install Edge manually
 
     drv.implicitly_wait(10)
     yield drv
