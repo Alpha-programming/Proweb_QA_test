9eacce93a1020615656376b76994b59816ed9e1f
Asadbek Kodirov <asadbekkodirov@Asadbeks-MacBook-Pro.local>
2025-11-17 14:39:58 +0900

    conftest.py changed

--- conftest.py
+++ conftest.py
@@ -1,31 +1,28 @@
-import pytest
 import os
-
+import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from selenium.webdriver.edge.service import Service as EdgeService
-
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.edge.options import Options as EdgeOptions
-
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 
 @pytest.fixture(params=["chrome", "firefox"])
 def driver(request):
     browser = request.param
+
     headless = os.getenv("CI") == "true"
 
     if browser == "chrome":
         options = ChromeOptions()
         if headless:
             options.add_argument("--headless=new")
+            options.add_argument("--disable-gpu")
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--remote-debugging-port=9222")
 
         service = ChromeService(ChromeDriverManager().install())
         drv = webdriver.Chrome(service=service, options=options)
@@ -38,10 +35,9 @@ def driver(request):
         service = FirefoxService(GeckoDriverManager().install())
         drv = webdriver.Firefox(service=service, options=options)
 
-    # Edge is DISABLED in CI because Microsoft blocks automated downloads
-    # If you want to enable locally, add param and install Edge manually
-
+    drv.maximize_window()
     drv.implicitly_wait(10)
     yield drv
     drv.quit()
+
 
