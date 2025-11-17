d4a8395b59e3bdebcd06e70b5b189d1078da2150
Asadbek Kodirov <asadbekkodirov@Asadbeks-MacBook-Pro.local>
2025-11-17 14:54:46 +0900

    conftest.py changed

--- conftest.py
+++ conftest.py
@@ -3,22 +3,22 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 import shutil
 
-@pytest.fixture(params=["firefox"])
+@pytest.fixture(params=["edge", "chrome", "firefox"])
 def driver(request):
     browser = request.param
 
-    # if browser == "edge" and not shutil.which("msedgedriver"):
-    #     pytest.skip("Edge WebDriver not found, skipping Edge tests")
-    # if browser == "chrome" and not shutil.which("chromedriver"):
-    #     pytest.skip("Chrome WebDriver not found, skipping Chrome tests")
+    if browser == "edge" and not shutil.which("msedgedriver"):
+        pytest.skip("Edge WebDriver not found, skipping Edge tests")
+    if browser == "chrome" and not shutil.which("chromedriver"):
+        pytest.skip("Chrome WebDriver not found, skipping Chrome tests")
     if browser == "firefox" and not shutil.which("geckodriver"):
         pytest.skip("Firefox WebDriver not found, skipping Firefox tests")
 
-    # if browser == "edge":
-    #     driver = webdriver.Edge()
-    # elif browser == "chrome":
-    #     driver = webdriver.Chrome()
-    if browser == "firefox":
+    if browser == "edge":
+        driver = webdriver.Edge()
+    elif browser == "chrome":
+        driver = webdriver.Chrome()
+    elif browser == "firefox":
         driver = webdriver.Firefox()
 
     driver.maximize_window()
